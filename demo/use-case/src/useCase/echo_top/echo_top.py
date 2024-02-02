fe06c69e1032695c4ae660249aa85cf35505ca07
Christian Kanesan <cfkanesan@gmail.com>
2024-02-02 15:39:34 +0100

    Merge pull request #84 from MeteoSwiss/feature/EchoTopUseCase
    Feature/echo top use case

---
+++ demo/use-case/src/useCase/echo_top/echo_top.py
@@ -0,0 +1,93 @@
+import datetime as dt
+import logging
+import sys
+from pathlib import Path
+
+import click
+from idpi import grib_decoder, mars, metadata, data_source
+from idpi.operators.destagger import destagger
+from idpi.operators.vertical_interpolation import interpolate_k2any
+
+from ..mch_model_data import mch_model_data
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+if mch_model_data.SOURCE == "FDB":
+    import pyfdb
+
+    client = pyfdb.FDB()
+elif mch_model_data.SOURCE == "FASTAPI":
+    from idpi import fdb_client
+
+    client = fdb_client.FDBClient(mch_model_data.FDB_HOST)
+else:
+    msg = f"Unsupported value for source: {mch_model_data.SOURCE}"
+    raise RuntimeError(msg)
+
+
+@click.command()
+@click.option(
+    "-r",
+    "--ref-time",
+    type=click.DateTime(["%Y%m%d%H"]),
+    default=dt.datetime(2023, 2, 1, 3),
+    help="Reference time, format: YYYYMMDDHH",
+)
+@click.option(
+    "-l",
+    "--lead-time",
+    type=click.INT,
+    default=0,
+    help="Lead time, type: int, default: 0",
+)
+@click.option(
+    "-o",
+    "--out-path",
+    type=click.Path(path_type=Path),
+    default=Path("echo_top_in_m.grib"),
+    help="Output path",
+)
+def calculate(ref_time: dt.datetime, lead_time: int, out_path: Path):
+    compute_echo_top(ref_time, lead_time, out_path)
+
+
+def compute_echo_top(ref_time: dt.datetime, lead_time: int, out_path: Path):
+    logger.info(
+        "Computing ECHOTOPinM for %s at T+%sh",
+        ref_time.strftime("%Y%m%d_%H"),
+        lead_time,
+    )
+
+    request = mars.Request(
+        ("HHL", "DBZ"),
+        date=ref_time.strftime("%Y%m%d"),
+        time=ref_time.strftime("%H00"),
+        expver="0001",
+        levelist=tuple(range(1, 82)),
+        number=tuple(range(11)),
+        step=lead_time,
+        levtype=mars.LevType.MODEL_LEVEL,
+        model=mars.Model.COSMO_1E,
+        stream=mars.Stream.ENS_FORECAST,
+        type=mars.Type.ENS_MEMBER,
+    )
+    ds = mch_model_data.get(request, ref_param_for_grid="HHL")
+
+    # Calculate ECHOTOPinM
+    hfl = destagger(ds["HHL"], "z")
+    echo_top = interpolate_k2any(hfl, "high_fold", ds["DBZ"], [15.0], hfl)
+
+    echo_top.attrs |= metadata.override(echo_top.message, shortName="ECHOTOPinM")
+    echo_top.attrs["vcoord_type"] = "echoTopInDBZ"
+
+    logger.info("Archiving ECHOTOPinM in FDB")
+    with data_source.cosmo_grib_defs():
+        with open("useCase/echo_top/output.grib", "w+b") as tmp:
+            grib_decoder.save(echo_top.rename({"DBZ": "z"}), tmp)
+            tmp.seek(0)
+
+            client.archive(tmp.read())
+
+    logger.info("Done")
--- demo/use-case/src/useCase/mch_model_data/mch_model_data.py
+++ demo/use-case/src/useCase/mch_model_data/mch_model_data.py
@@ -10,10 +10,15 @@ from idpi.grib_decoder import GribReader
 from polytope.api import Client
 
 
+logger = logging.getLogger(__name__)
+FDB_HOST = os.environ.get("FDB_HOST", "http://balfrin-ln002:8989")
+SOURCE = os.environ.get("MCH_MODEL_DATA_SOURCE")
+
+
 def get(request: mars.Request, ref_param_for_grid: str) -> dict[str, xr.DataArray]:
-    if os.environ.get("MCH_MODEL_DATA_SOURCE") == "FDB":
+    if SOURCE == "FDB":
         if "FDB5_CONFIG" not in os.environ and "FDB5_CONFIG_FILE" not in os.environ:
-            logging.error(
+            logger.error(
                 "Required environment variables for FDB are not set. Define one of 'FDB5_CONFIG' or 'FDB5_CONFIG_FILE'"
             )
             raise RuntimeError(
@@ -21,6 +26,8 @@ def get(request: mars.Request, ref_param_for_grid: str) -> dict[str, xr.DataArra
             )
 
         return get_from_fdb(request, ref_param_for_grid)
+    elif SOURCE == "FASTAPI":
+        return get_from_fdb_client(request, ref_param_for_grid)
     else:
         return get_from_polytope(request, ref_param_for_grid)
 
@@ -40,9 +47,29 @@ def get_from_polytope(
 def get_from_fdb(
     request: mars.Request, ref_param_for_grid: str
 ) -> dict[str, xr.DataArray]:
-    reader = GribReader(ref_param=ref_param_for_grid, source=DataSource())
+    base = request.to_fdb()
+    reader = GribReader(
+        ref_param=base | {"param": ref_param_for_grid}, source=DataSource()
+    )
 
     if not isinstance(request.param, str):
-        return reader.load({params: request.to_fdb() for params in request.param})
+        return reader.load({param: base | {"param": param} for param in request.param})
 
-    return reader.load({request.param: request.to_fdb()})
+    return reader.load({request.param: base | {"param": request.param}})
+
+
+def get_from_fdb_client(
+    request: mars.Request, ref_param_for_grid: str
+) -> dict[str, xr.DataArray]:
+    from idpi import fdb_client
+
+    base = request.to_fdb()
+    client = fdb_client.FDBClient(FDB_HOST)
+    reader = GribReader(
+        ref_param=base | {"param": ref_param_for_grid}, source=DataSource(client=client)
+    )
+
+    if not isinstance(request.param, str):
+        return reader.load({param: base | {"param": param} for param in request.param})
+
+    return reader.load({request.param: base | {"param": request.param}})
