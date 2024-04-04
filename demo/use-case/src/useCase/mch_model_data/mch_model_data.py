3bc03f3036c994b28dafbb81fa1024f62d87e2c4
Stefan Friedli <stefan.friedli@zuehlke.com>
2024-04-04 11:56:01 +0200

    Merge pull request #107 from MeteoSwiss/feature/update_idpi
    Feature/update idpi

--- demo/use-case/src/useCase/mch_model_data/mch_model_data.py
+++ demo/use-case/src/useCase/mch_model_data/mch_model_data.py
@@ -1,14 +1,10 @@
 import logging
 import os
-import uuid
-from pathlib import Path
 
 import xarray as xr
 from idpi import mars
 from idpi.data_source import DataSource
-from idpi.grib_decoder import GribReader
-from polytope.api import Client
-
+from idpi.grib_decoder import load
 
 logger = logging.getLogger(__name__)
 SOURCE = os.environ.get("MCH_MODEL_DATA_SOURCE")
@@ -30,22 +26,8 @@ def get(request: mars.Request) -> dict[str, xr.DataArray]:
 
 
 def get_from_polytope(request: mars.Request) -> dict[str, xr.DataArray]:
-    polytope_client = Client()
-    filename = f"{uuid.uuid4()}.grib"
-
-    polytope_client.retrieve("mch", request.to_fdb(), filename)
-    datafiles = [Path(filename)]
-    reader = GribReader.from_files(datafiles)
-    if not isinstance(request.param, str):
-        return reader.load_fieldnames(request.param)
-    return reader.load_fieldnames([request.param])
+    return load(DataSource(polytope_collection="mch"), request=request)
 
 
 def get_from_fdb(request: mars.Request) -> dict[str, xr.DataArray]:
-    base = request.to_fdb()
-    reader = GribReader(source=DataSource())
-
-    if not isinstance(request.param, str):
-        return reader.load({param: base | {"param": param} for param in request.param})
-
-    return reader.load({request.param: base | {"param": request.param}})
+    return load(DataSource(), request=request)
