6ec614d98f23486b79499827a072b9178dd52eed
nnellen <193914323+nnellen@users.noreply.github.com>
2025-07-31 16:34:01 +0200

    first draft

---
+++ internal_use/radiation.py
@@ -0,0 +1,34 @@
+import earthkit.data as ekd
+import time
+
+from config import real_fdb_config
+
+# Request to retrieve ASWDIFD_S at surface level between the 1st january and 31st december of 2021 hourly
+req = {
+    "date": "20120101/to/20121231",
+    "time": "0000",
+    "stream": "enfo",
+    "class": "od",
+    "expver": "0001",
+    "model": "icon-ch1-eps",
+    "type": "cf",
+    "levtype": "sfc",
+    "param": "500481",
+    "step": "0/to/24/by/1",
+}
+
+# Time data extraction
+start = time.time()
+
+# Load data as a stream, otherwise it might be too much data
+fs = ekd.from_source("fdb", req, config=real_fdb_config, stream=True)
+
+# Convert each field to a xarray.Dataset and print it variables and the date
+for f in fs.group_by("date"):
+    ds = f.to_xarray()
+    print(ds.data_vars)
+    print(f"Date : {ds.date}")
+end = time.time()
+
+# Print total extraction time
+print(f"Total extraction time (s): {end-start}")
---
+++ internal_use/wind_10M.py
@@ -0,0 +1,34 @@
+import earthkit.data as ekd
+import time
+
+from config import real_fdb_config
+
+# Request to retrieve the two wind parameters U_10M and V_10M at surface level on the 1st of january 2012 hourly
+req = {
+    "date": "20120201",
+    "time": "0000",
+    "stream": "enfo",
+    "class": "od",
+    "expver": "0001",
+    "model": "icon-ch1-eps",
+    "type": "cf",
+    "levtype": "sfc",
+    "param": "500027/500029",
+    "step": "0/to/24/by/1",
+}
+
+# Time data extraction
+start = time.time()
+
+# Load data as a stream, otherwise it might be too much data
+fs = ekd.from_source("fdb", req, config=real_fdb_config, stream=True)
+
+# Convert each field to a xarray.Dataset and print it variables and the date
+for f in fs.group_by("date"):
+    ds = f.to_xarray()
+    print(ds.data_vars)
+    print(f"Date : {ds.date}")
+end = time.time()
+
+# Print total extraction time
+print(f"Total extraction time : {end-start}")
