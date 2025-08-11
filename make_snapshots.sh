346bafc26e676d2d0d03e07fe8db36bb678b8f47
cosunae <carlos.osuna@meteoswiss.ch>
2025-08-11 11:48:42 +0200

    Update make_snapshots.sh
    Co-authored-by: Nina Burgdorfer <83002751+ninaburg@users.noreply.github.com>

--- make_snapshots.sh
+++ make_snapshots.sh
@@ -24,7 +24,7 @@ done
 if [ "${make_snapshots}" = true ] ; then
     mkdir tmp
     for filename in notebooks/*.ipynb; do
-	if grep -E -q 'EmailKey|Bearer' file.txt; then
+	if grep -E -q 'EmailKey|Bearer' "$filename"; then
             echo "Token found in the notebook. Exiting script."
             exit 1
         fi
