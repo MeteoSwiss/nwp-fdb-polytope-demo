87d46203a69859daa44ebd72c5a4b18490dc0d98
Carlos Osuna <carlos.osuna@meteoswiss.ch>
2025-09-01 23:30:52 +0200

    adding files

--- internal_use/README.md
+++ internal_use/README.md
@@ -7,11 +7,18 @@
 - [radiation.py](radiation.py): Retrieve two parameters at surface level across 10 days
 - [wind_10M.py](wind_10M.py): Retrieve two parameters at surface level across a day
 - [wind_multi_levels.py](wind_multi_levels.py): Retrieve two parameters on multiple model levels across a day
+- [regrid.py](regrid.py): Retrieve multiple requests and regrid the data.
 
 **MARS:** Example MARS requests to retrieve data via the FDB CLI
 - [request_surface.mars](request_surface.mars): Retrieve one parameter at surface level
 - [request_model_level.mars](request_model_level.mars): Retrieve one parameter on multiple model levels
 
 ## Instructions
-
 - [FDB for NWP Data Access at CSCS](https://meteoswiss.atlassian.net/wiki/x/poR-Ew)
+
+### Installation Instructions for regrid.py
+
+```sh
+poetry install
+poetry run python regrid.py
+```
---
+++ internal_use/regrid.py
@@ -0,0 +1,244 @@
+import earthkit.data as ekd
+
+from config import real_fdb_config
+import yaml
+from meteodatalab.operators import regrid
+from meteodatalab import icon_grid
+import xarray as xr
+from uuid import UUID
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+# Convert each field to a xarray.Dataset and print the available parameters and the date of the dataset.
+with open(script_dir / "profile.yaml", "r") as file:
+    profile = yaml.safe_load(file)
+
+# Convert each field to a xarray.Dataset and print the available parameters and the date of the dataset.
+with open(script_dir / "profile_2d.yaml", "r") as file:
+    profile_2d = yaml.safe_load(file)
+
+
+mapfile = "/user-environment/env/fdb/share/metkit/paramids.yaml"
+with open(mapfile, "r") as file:
+    param_map_ = yaml.safe_load(file)
+    param_map = {}
+    for key, val in param_map_.items():
+        if int(key) < 500000:
+            continue
+        param_map[val[0]] = key
+
+
+def regrid_dataset(ds, grid_params, x_y_as_dims):
+    target_grid = regrid.RegularGrid.parse_regrid_operator(grid_params)
+
+    regridded_ds = xr.Dataset(
+        {var: regrid.iconremap(ds[var], target_grid) for var in ds.data_vars}
+    )
+
+    # Add x/y as coordinates
+    regridded_ds = regridded_ds.assign_coords(x=target_grid.x, y=target_grid.y)
+
+    #    if not x_y_as_dims:
+    #        return regridded_ds.swap_dims({"y": "lat", "x": "lon"})
+
+    return regridded_ds
+
+
+def var_to_paramid(param_names):
+    return [param_map[param.lower()] for param in param_names]
+
+
+def add_icon_grid(array):
+    hcoords = icon_grid.load_grid_from_balfrin()(UUID(array.attrs["uuidOfHGrid"]))
+
+    array = array.rename({"values": "cell"})
+    array = array.assign_coords(
+        lon=("cell", hcoords["lon"].data), lat=("cell", hcoords["lat"].data)
+    )
+
+    return array
+
+
+requests = {
+    "cons": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "sfc",
+            "step": "0",
+        },
+        "vars": ["HSURF"],
+        "steps": None,
+        "levels": None,
+    },
+    "sfc": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "sfc",
+            "step": "0/to/24/by/1",
+        },
+        "vars": [
+            "ALB_RAD",
+            "CLCT",
+            "DURSUN",
+            "H_SNOW",
+            "PMSL",
+            "VMAX_10M",
+            "U_10M",
+            "V_10M",
+            "SNOW_GSP",
+            "GRAU_GSP",
+            "SKT",
+            "T_G",
+            "T_2M",
+            "TD_2M",
+            "TOT_PREC",
+            "PS",
+            "ASOB_S",
+            "ATHD_S",
+            "ATHU_S",
+            "ALHFL_S",
+            "ASHFL_S",
+            "RAIN_GSP",
+            "RUNOFF_G",
+            "W_SNOW",
+            "W_I",
+            "T_G",
+            "TWATER",
+            "CAPE_ML",
+            "CAPE_MU",
+            "HZEROCL",
+            "ASWDIFU_S",
+            #    "WSPD_1h",
+            "ASWDIR_S",
+            "ASWDIFD_S",
+        ],
+        "steps": 25,
+        "levels": None,
+    },
+    "dp1": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "dp",
+            "levelist": ["0", "0.005", "0.02", "0.06", "0.18", "0.54", "1.62", "4.86"],
+            "step": "0/to/24/by/1",
+        },
+        "vars": ["T_SO"],
+        "steps": 25,
+        "levels": 8,
+    },
+    "dp2": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "dp",
+            "levelist": ["0", "0.01", "0.03", "0.09", "0.27", "0.81", "2.43", "7.29"],
+            "step": "0/to/24/by/1",
+        },
+        "vars": ["W_SO"],
+        "steps": 25,
+        "levels": 8,
+    },
+    "10m_sfc": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "sfc",
+            "step": "0m/to/1441m/by/10m",
+        },
+        "vars": ["TOT_PREC", "DHAIL_MX", "DBZ_CMAX"],
+        "steps": 145,
+        "levels": None,
+    },
+    "10m_avg": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "sfc",
+            "step": "0m/to/1441m/by/10m",
+        },
+        "vars": ["U_10M_AV", "V_10M_AV"],
+        "steps": 145,
+        "levels": None,
+    },
+    "ml": {
+        "request": {
+            "time": "0000",
+            "stream": "enfo",
+            "class": "od",
+            "expver": "0001",
+            "model": "icon-ch1-eps",
+            "type": "cf",
+            "levtype": "ml",
+            "levelist": "74/to/80",
+            "step": "0/to/24/by/1",
+        },
+        "vars": ["U", "V"],
+        "steps": 25,
+        "levels": 7,
+    },
+}
+
+for key, req_info in requests.items():
+
+    print(f"Processing {key}")
+    req = req_info["request"]
+
+    req["date"] = "20100917/to/20100920"
+    req["param"] = var_to_paramid(req_info["vars"])
+
+    # Load data as a stream, otherwise it might not fit in memory
+    fs = ekd.from_source("fdb", req, config=real_fdb_config, stream=True)
+
+    # Convert each field to a xarray.Dataset and print the available parameters and the date of the dataset.
+    for f in fs.group_by("date"):
+        ds = f.to_xarray(profile="grib", **profile)
+
+        for var in ds:
+            ds[var] = add_icon_grid(ds[var])
+            if key == "10m_avg":
+                # Build 1h averages (still sampling at 10')
+                ds[var] = ds[var].resample(lead_time="1h").mean()
+
+        if req_info["levels"] and len(ds.coords["z"]) != req_info["levels"]:
+            raise RuntimeError("error finding all levelist")
+        if req_info["steps"] and len(ds.coords["lead_time"]) != req_info["steps"]:
+            raise RuntimeError("error finding all steps")
+
+        out_regrid_target = "swiss,549500,149500,650500,250500,1000,1000"
+        swiss_ds = regrid_dataset(ds, out_regrid_target, False)
+
+        for var in swiss_ds:
+            vals = ds.coords["valid_time"].dt.strftime("%Y%m").values
+            assert len(vals) == 1
+            assert all(x == vals[0][0] for x in vals[0])
+            valid_time_month = vals[0][0]
+
+            filename = f"ds_{var}_steps{req_info['steps']}_{valid_time_month}"
+            swiss_ds.earthkit.to_netcdf(filename, mode="a")
