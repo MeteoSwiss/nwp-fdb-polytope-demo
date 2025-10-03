ced29187afb0274cb9f87a589f5b673a3410972e
nnellen <193914323+nnellen@users.noreply.github.com>
2025-10-03 16:25:10 +0200

    update description

--- internal_use/wind_multi_levels.py
+++ internal_use/wind_multi_levels.py
@@ -3,7 +3,7 @@ import time
 
 from config import real_fdb_config
 
-# Request to retrieve horizontal wind parameters U and V at model levels 74 to 80 on the 2nd of January 2012, hourly
+# Request to retrieve horizontal wind parameters U and V at model levels 74 to 80 on the 2nd of January 2010, hourly
 req = {
     "date": "20100102",
     "time": "0000",
